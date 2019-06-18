c161cccab0bcdbe9e11199ec4145bfeee730e885
SanderV1992 <sander.vergeles@gmail.com>
2019-06-18 08:54:15 +0300

    Fix sub-modules

--- application/src/main/java/com/example/demo/BurgerFinderApplication.java
+++ application/src/main/java/com/example/demo/BurgerFinderApplication.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
+import com.example.burgerapi.BurgerAutoConfiguration;
+import com.example.foursquareapi.FoursquareAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.example.demo",
-        "com.example.burgerapi",
-        "com.example.foursquareapi"
-},
-        scanBasePackageClasses = {}
-)
+@Import({ BurgerAutoConfiguration.class, FoursquareAutoConfiguration.class})
+@SpringBootApplication
 public class BurgerFinderApplication {
 
     public static void main(String[] args) {
